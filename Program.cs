d884a3a2f5151b2143169c830a6de809e3d4f523
Kauê Alcantara Rocha <118750523+garotodeprogramacao@users.noreply.github.com>
2023-12-13 23:43:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _17_08_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+            do
+            {
+                Console.Write("Insira um número par: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+            } while ();
+             
+        }
+    }
+}
